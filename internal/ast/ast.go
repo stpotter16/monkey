b6c9cb3b17a71d85036f670dc3e0f226baaabdd2
Sam Potter <spotter@huntenergy.com>
2025-12-11 13:23:59 -0500

    Add LetStatement type to ast

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "monkey/internal/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -17,6 +19,23 @@ type Expression interface {
 type Program struct {
 	Statements []Statement
 }
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
